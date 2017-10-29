5f44f1f269aeb926062742007b981d2edf901049
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-10-28 21:39:27 -0400

    add dc and tinyurl

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
-from ext import embedtobox
+from ext.color import ColorNames
+from PIL import Image
+import io
 
 
 class Utility:
@@ -18,66 +20,31 @@ class Utility:
         await ctx.send('`Selfbot Logging out...`')
         await self.bot.logout()
 
-    @commands.command(name='help')
-    async def new_help_command(self, ctx, *commands: str):
-        '''Shows this message.'''
-        destination = ctx.message.author if self.bot.pm_help else ctx.message.channel
+    @commands.command()
+    async def dcolor(self, ctx, *, url):
+        '''For testing to see if it works'''
+        await ctx.message.delete()
+        color = await ctx.get_dominant_color(url)
+        string_col = ColorNames.color_name(str(color))
+        info = f'`{str(color)}`\n`{color.to_rgb()}`\n`{str(string_col)}`'
+        em = discord.Embed(color=color, title='Dominant Color', description=info)
+        em.set_thumbnail(url=url)
+        file = io.BytesIO()
+        Image.new('RGB', (200, 90), color.to_rgb()).save(file, format='PNG')
+        file.seek(0)
+        em.set_image(url="attachment://color.png")
+        await ctx.send(file=discord.File(file, 'color.png'), embed=em)
 
-        def repl(obj):
-            return self.bot._mentions_transforms.get(obj.group(0), '')
-
-        # help by itself just lists commands
-        if len(commands) == 0:
-            pages = await self.bot.formatter.format_help_for(ctx, self.bot)
-        elif len(commands) == 1:
-            # try to see if it is a cog name
-            name = self.bot._mention_pattern.sub(repl, commands[0])
-            command = None
-            if name in self.bot.cogs:
-                command = self.bot.cogs[name]
-            else:
-                command = self.bot.all_commands.get(name)
-                if command is None:
-                    await destination.send(self.bot.command_not_found.format(name))
-                    return
-
-            pages = await self.bot.formatter.format_help_for(ctx, command)
-        else:
-            name = self.bot._mention_pattern.sub(repl, commands[0])
-            command = self.bot.all_commands.get(name)
-            if command is None:
-                await destination.send(self.bot.command_not_found.format(name))
-                return
-
-            for key in commands[1:]:
-                try:
-                    key = self.bot._mention_pattern.sub(repl, key)
-                    command = command.all_commands.get(key)
-                    if command is None:
-                        await destination.send(self.bot.command_not_found.format(key))
-                        return
-                except AttributeError:
-                    await destination.send(self.bot.command_has_no_subcommands.format(command, key))
-                    return
-
-            pages = await self.bot.formatter.format_help_for(ctx, command)
-
-        if self.bot.pm_help is None:
-            characters = sum(map(lambda l: len(l), pages))
-            # modify destination based on length of pages.
-            if characters > 1000:
-                destination = ctx.message.author
-
-        color = await ctx.get_dominant_color(ctx.author.avatar_url)
-
-        for embed in pages:
-            embed.color = color
-            try:
-                await ctx.send(embed=embed)
-            except discord.HTTPException:
-                em_list = await embedtobox.etb(embed)
-                for page in em_list:
-                    await ctx.send(page)
+    @commands.command()
+    async def tinyurl(self, ctx, *, link: str):
+        await ctx.message.delete()
+        url = 'http://tinyurl.com/api-create.php?url=' + link
+        async with ctx.session.get(url) as resp:
+            new = await resp.text()
+        emb = discord.Embed(color=await ctx.get_dominant_color(ctx.author.avatar_url))
+        emb.add_field(name="Original Link", value=link, inline=False)
+        emb.add_field(name="Shortened Link", value=new, inline=False)
+        await ctx.send(embed=emb)
 
 
 def setup(bot):
