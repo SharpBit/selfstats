c4764cdae65aa0c324268bfee59f7b39c3bd048c
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-10-29 11:01:32 -0400

    more in profile cmd

--- cogs/profile.py
+++ cogs/profile.py
@@ -1,14 +1,12 @@
 import discord
 from discord.ext import commands
 import crasync
+import aiohttp
 import json
 import os
-from PIL import Image
-import io
-from ext.colors import ColorNames
 
 
-class PlayerInfo:
+class Profile:
 
     def __init__(self, bot):
         self.bot = bot
@@ -37,16 +35,45 @@ class PlayerInfo:
             em.description = 'Either the API is down or that\'s an invalid tag.'
             return await ctx.send(embed=em)
 
+        try:
+            clan_name = profile.clan.name
+            clan_badge = profile.clan_badge_url
+            clan_region = profile.clan.region
+            clan_members = str(len(profile.clan.members)) + '/50'
+        except:
+            pass
+
+        name = profile.profile.name
+        level = str(profile.profile.level)
+        experience = str(profile.profile.experience(0)) + '/' + str(profile.profile.experience(1))
+        trophies = str(profile.profile.current_trophies)
+        highest_trophs = str(profile.profile.highest_trophies)
+        legend_trophies = str(profile.profile.legend_trophies)
+        arena = profile.arena.name + ', Arena ' + profile.arena.number
+
         em.title = profile.name
         em.description = f'#{tag}'
         em.url = f'http://cr-api.com/profile/{tag}'
         try:
-            em.set_author(name='Profile', icon_url=profile.clan_badge_url)
+            em.set_author(name='Profile', icon_url=clan_badge)
+            em.add_field(name='Player Name', value=name)
+            em.add_field(name='Level', value=level + '(' + experience + ')')
+            em.add_field(name='Trophies', value='Current: ' + trophies +
+                         '\nHighest: ' + highest_trophs + '\nLegend: ' + legend_trophies)
+            em.add_field(name='Arena', value=arena)
+            em.add_field(name='Clan', value='Name: ' + clan_name + '\nRegion: ' +
+                         clan_region + '\nMembers: ' + clan_members)
         except:
             em.set_author(name='Profile')
+            em.add_field(name='Player Name', value=name)
+            em.add_field(name='Level', value=level + '(' + experience + ')')
+            em.add_field(name='Trophies', value='Current: ' + trophies +
+                         '\nHighest: ' + highest_trophs + '\nLegend: ' + legend_trophies)
+            em.add_field(name='Arena', value=arena)
+            em.add_field(name='Clan', value='No clan')
 
         await ctx.send(embed=em)
 
 
 def setup(bot):
-    bot.add_cog(PlayerInfo(bot))
+    bot.add_cog(Profile(bot))
