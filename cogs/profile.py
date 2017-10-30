229624fb7ce5401e52caa171f6cb5350d52a4704
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-10-29 20:36:34 -0400

    url command and better aesthetics for trophies command

--- cogs/profile.py
+++ cogs/profile.py
@@ -139,10 +139,34 @@ class Profile:
 
         em.title = profile.name
         em.set_author(name='Trophies', icon_url=profile.arena.image_url)
-        em.description = 'Trophies: ' + trophies + '\nPersonal Best: ' + \
-            highest_trophies + '\nLegend Trophies: ' + legend_trophies
+        em.description = 'Trophies: `' + trophies + '`\nPersonal Best: `' + \
+            highest_trophies + '`\nLegend Trophies: `' + legend_trophies + '`'
         em.set_thumbnail(
             url='http://vignette1.wikia.nocookie.net/clashroyale/images/7/7c/LegendTrophy.png/revision/latest?cb=20160305151655')
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
+                      icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
+
+        await ctx.send(embed=em)
+
+    @commands.command()
+    async def weburl(self, ctx, tag=None):
+        '''Get the cr-api.com url for a tag'''
+        em = discord.Embed(title='cr-api.com URL')
+        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
+        if tag is None:
+            tag = self.tag
+            if tag is None:
+                em.description - 'Please add `TAG` to your config.'
+                return await ctx.send(embed=em)
+        try:
+            profile = await self.client.get_profile(tag)
+        except:
+            em.description = 'Either the API is down or that\'s an invalid tag.'
+            return await ctx.send(embed=em)
+
+        em.url = 'http://cr-api.com/profile/{tag}'
+        em.title = profile.name
+        em.add_field(name='URL', value='http://cr-api.com/profile/{tag}')
         em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
                       icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
 
