cff932656f9cce2527502e781562544d9b2690f0
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-03 16:45:11 -0400

    hopefully this works

--- cogs/profile.py
+++ cogs/profile.py
@@ -49,10 +49,10 @@ class Profile:
         else:
             global_rank = 'N/A'
 
-        #chest_cycle = ',\n'.join([profile.get_chest(x) for x in range(10)])
-        #sm = str(cycle.super_magical)
-        #legend = str(cycle.legendary)
-        #epic = str(cycle.epic)
+        chest_cycle = ',\n'.join([profile.get_chest(x) for x in range(10)])
+        sm = str(cycle.super_magical)
+        legend = str(cycle.legendary)
+        epic = str(cycle.epic)
 
         level = str(profile.level)
         experience = str(profile.experience[0]) + '/' + str(profile.experience[1])
@@ -80,60 +80,37 @@ class Profile:
         em.url = f'http://cr-api.com/profile/{tag}'
         if clan.name is not None:
             em.set_author(name='Profile', icon_url=clan.badge_url)
+        else:
+            em.set_author(name='Profile', icon_url='https://i.imgur.com/Y3uXsgj.png')
 
-            em.add_field(name='Level', value=level + ' (' + experience + ')')
-            em.add_field(name='Arena', value=arena)
+        em.add_field(name='Level', value=level + ' (' + experience + ')')
+        em.add_field(name='Arena', value=arena)
 
-            em.add_field(name='Trophies', value=trophies)
-            em.add_field(name='Personal Best', value=highest_trophies)
-            em.add_field(name='Global Rank', value=global_rank)
-            em.add_field(name='Total Donations', value=donations)
-            em.add_field(name='Win-Loss Percentage', value=win_percent)
-            em.add_field(name='Max Challenge Wins', value=str(profile.max_wins))
-            em.add_field(name='Favorite Card', value=profile.favourite_card)
-            em.add_field(name='Game Record', value=record)
-            em.add_field(name='Win Streak', value=win_streak)
+        em.add_field(name='Trophies', value=trophies)
+        em.add_field(name='Personal Best', value=highest_trophies)
+        em.add_field(name='Global Rank', value=global_rank)
+        em.add_field(name='Total Donations', value=donations)
+        em.add_field(name='Win-Loss Percentage', value=win_percent)
+        em.add_field(name='Max Challenge Wins', value=str(profile.max_wins))
+        em.add_field(name='Favorite Card', value=profile.favourite_card)
+        em.add_field(name='Game Record', value=record)
+        em.add_field(name='Win Streak', value=win_streak)
 
+        if clan.name is not None:
             em.add_field(name='Clan Info', value=clan.name +
                          '\n#' + clan.tag + '\n' + profile.clan_role)
-
-            em.add_field(name='Tournament Cards Won', value=str(profile.tournament_cards_won))
-            em.add_field(name='Challenge Cards Won', value=str(profile.challenge_cards_won))
-            em.add_field(name='Battle Deck', value=fmt)
-            # em.add_field(name='Chests', value=chest_cycle + '\nSuper Magical: ' +
-            # sm + '\nLegendary: ' + legend + '\nEpic: ' + epic)
-
-            em.set_thumbnail(url=profile.arena.image_url)
-            em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
-                          icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
-
         else:
-            em.set_author(name='Profile')
-            em.add_field(name='Level', value=level + '(' + experience + ')')
-            em.add_field(name='Arena', value=arena)
-
-            em.add_field(name='Trophies', value=trophies)
-            em.add_field(name='Personal Best', value=highest_trophies)
-            em.add_field(name='Global Rank', value=global_rank)
-            em.add_field(name='Total Donations', value=donations)
-            em.add_field(name='Win-Loss Percentage', value=win_percent)
-            em.add_field(name='Max Challenge Wins', value=str(profile.max_wins))
-            em.add_field(name='Favorite Card', value=profile.favourite_card)
-            em.add_field(name='Game Record', value=record)
-            em.add_field(name='Win Streak', value=win_streak)
-
             em.add_field(name='Clan Info', value='No clan')
-            em.add_field(name='Tournament Cards Won', value=str(profile.tournament_cards_won))
-            em.add_field(name='Challenge Cards Won', value=str(profile.challenge_cards_won))
-            em.add_field(name='Battle Deck', value=fmt)
-            # em.add_field(name='Chests', value=chest_cycle + '\nSuper Magical: ' +
-            # sm + '\nLegendary: ' + legend + '\nEpic: ' + epic)
 
-            em.set_thumbnail(url=profile.arena.image_url)
-            em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
-                          icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
+        em.add_field(name='Tournament Cards Won', value=str(profile.tournament_cards_won))
+        em.add_field(name='Challenge Cards Won', value=str(profile.challenge_cards_won))
+        em.add_field(name='Battle Deck', value=fmt)
+        em.add_field(name='Chests', value=chest_cycle + '\nSuper Magical: ' +
+                     sm + '\nLegendary: ' + legend + '\nEpic: ' + epic)
 
-        await ctx.send(embed=em)
+        em.set_thumbnail(url=profile.arena.image_url)
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api v{crasync.__version__}',
+                      icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
 
     @commands.command()
     async def trophies(self, ctx, tag=None):
@@ -161,7 +138,7 @@ class Profile:
             highest_trophies + '`\nLegend Trophies: `' + legend_trophies + '`'
         em.set_thumbnail(
             url='http://vignette1.wikia.nocookie.net/clashroyale/images/7/7c/LegendTrophy.png/revision/latest?cb=20160305151655')
-        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api v{crasync.__version__}',
                       icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
 
         await ctx.send(embed=em)
@@ -185,7 +162,7 @@ class Profile:
         em.url = f'http://cr-api.com/profile/{tag}'
         em.title = profile.name
         em.add_field(name='URL', value=f'http://cr-api.com/profile/{tag}')
-        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api v{crasync.__version__}',
                       icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
 
         await ctx.send(embed=em)
