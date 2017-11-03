d6b68f8105d02de5710797a7ad2cbaa4d29963ff
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-03 18:53:52 -0400

    reformat profile and add battle deck cmd

--- cogs/profile.py
+++ cogs/profile.py
@@ -20,12 +20,7 @@ class Profile:
         self.tag = os.environ.get('TAG') or tag
         self.client = crasync.Client()
 
-    @commands.command()
-    async def profile(self, ctx, tag=None):
-        '''Fetch a Clash Royale Profile'''
-        em = discord.Embed(title='Profile')
-        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
-
+    def load_profile(self, ctx, tag=None):
         if tag is None:
             tag = self.tag
             if tag is None:
@@ -37,23 +32,31 @@ class Profile:
             em.description = 'Either the API is down or that\'s an invalid tag.'
             return await ctx.send(embed=em)
 
+        return profile
+
+    @commands.command()
+    async def profile(self, ctx, tag=None):
+        '''Fetch a Clash Royale Profile'''
+        em = discord.Embed(title='Profile')
+        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
+
+        self.load_profile(ctx, tag)
+
         try:
             clan = await profile.get_clan()
         except ValueError:
             pass
-
-        # cycle = profile.chest_cycle
 
         if profile.global_rank is not None:
             global_rank = str(profile.global_rank)
         else:
             global_rank = 'N/A'
 
-        cycle = ',\n'.join([profile.get_chest(x) for x in range(10)])
-        c = profile.chest_cycle
-        sm = str(c.super_magical)
-        legend = str(c.legendary)
-        epic = str(c.epic)
+        # cycle = ',\n'.join([profile.get_chest(x) for x in range(10)])
+        # c = profile.chest_cycle
+        # sm = str(c.super_magical)
+        # legend = str(c.legendary)
+        # epic = str(c.epic)
 
         level = str(profile.level)
         experience = str(profile.experience[0]) + '/' + str(profile.experience[1])
@@ -87,15 +90,14 @@ class Profile:
         em.add_field(name='Level', value=level + ' (' + experience + ')')
         em.add_field(name='Arena', value=arena)
 
-        em.add_field(name='Trophies', value=trophies)
-        em.add_field(name='Personal Best', value=highest_trophies)
+        em.add_field(name='Trophies (Current/Best/Legend)', value=trophies +
+                     '/' + highest_trophies + '/' + legend_trophies)
         em.add_field(name='Global Rank', value=global_rank)
         em.add_field(name='Total Donations', value=donations)
         em.add_field(name='Win Percentage', value=win_percent)
         em.add_field(name='Max Challenge Wins', value=str(profile.max_wins))
         em.add_field(name='Favorite Card', value=profile.favourite_card)
-        em.add_field(name='Game Record', value=record)
-        em.add_field(name='Win Streak', value=win_streak)
+        em.add_field(name='Game Record (Win Streak)', value=record + '(' + win_streak + ')')
 
         if clan.name is not None:
             em.add_field(name='Clan Info', value=clan.name +
@@ -106,8 +108,8 @@ class Profile:
         em.add_field(name='Tournament Cards Won', value=str(profile.tournament_cards_won))
         em.add_field(name='Challenge Cards Won', value=str(profile.challenge_cards_won))
         em.add_field(name='Battle Deck', value=fmt)
-        em.add_field(name='Chests', value=cycle + '\nSuper Magical: ' +
-                     sm + '\nLegendary: ' + legend + '\nEpic: ' + epic)
+        # em.add_field(name='Chests', value=cycle + '\nSuper Magical: ' +
+        #             sm + '\nLegendary: ' + legend + '\nEpic: ' + epic)
 
         em.set_thumbnail(url=profile.arena.image_url)
         em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
@@ -118,23 +120,16 @@ class Profile:
         '''See your current, record, and legend trophies'''
         em = discord.Embed(title='Trophies')
         em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
-        if tag is None:
-            tag = self.tag
-            if tag is None:
-                em.description - 'Please add `TAG` to your config.'
-                return await ctx.send(embed=em)
-        try:
-            profile = await self.client.get_profile(tag)
-        except:
-            em.description = 'Either the API is down or that\'s an invalid tag.'
-            return await ctx.send(embed=em)
+
+        self.load_profile(ctx, tag)
 
         trophies = str(profile.current_trophies)
         highest_trophies = str(profile.highest_trophies)
         legend_trophies = str(profile.legend_trophies)
 
         em.title = profile.name
-        em.set_author(name='Trophies', icon_url=profile.arena.image_url)
+        em.set_author(
+            name='Trophies', icon_url='http://clashroyalehack1.com/wp-content/uploads/2017/06/coctrophy.png')
         em.description = 'Trophies: `' + trophies + '`\nPersonal Best: `' + \
             highest_trophies + '`\nLegend Trophies: `' + legend_trophies + '`'
         em.set_thumbnail(
@@ -143,6 +138,29 @@ class Profile:
                       icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
 
         await ctx.send(embed=em)
+
+    @commands.command()
+    async def deck(self, ctx, tag=None):
+        '''View a player's deck'''
+        em = discord.Embed(title='Battle Deck')
+        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
+
+        self.load_profile(ctx, tag)
+
+        deck = profile.deck
+        deck_levels = {}
+        for card in deck:
+            deck_levels[card.name] = 'Lvl ' + str(card.level)
+        fmt = ''
+        for key, value in deck_levels.items():
+            fmt += key + ': ' + value + '\n'
+
+        em.title = profile.name
+        em.set_author(name='Battle Deck', icon_url=ctx.author.avatar_url)
+        em.description = fmt
+        em.set_thumbnail(
+            url='https://i.pinimg.com/736x/46/11/09/46110956bb8b5e3fc5e01ad566a2f99d--swords-wer.jpg')
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api')
 
     @commands.command()
     async def weburl(self, ctx, tag=None):
