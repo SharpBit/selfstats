9520bace4417e2d2ef3e4794eab1771e9cc5ba6f
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-03 18:34:08 -0400

    hopefully indent fixed

--- cogs/profile.py
+++ cogs/profile.py
@@ -49,11 +49,11 @@ class Profile:
         else:
             global_rank = 'N/A'
 
-        # cycle = ',\n'.join([profile.get_chest(x) for x in range(10)])
-        # c = profile.chest_cycle
-        # sm = str(c.super_magical)
-        # legend = str(c.legendary)
-        # epic = str(c.epic)
+        cycle = ',\n'.join([profile.get_chest(x) for x in range(10)])
+        c = profile.chest_cycle
+        sm = str(c.super_magical)
+        legend = str(c.legendary)
+        epic = str(c.epic)
 
         level = str(profile.level)
         experience = str(profile.experience[0]) + '/' + str(profile.experience[1])
@@ -106,36 +106,36 @@ class Profile:
         em.add_field(name='Tournament Cards Won', value=str(profile.tournament_cards_won))
         em.add_field(name='Challenge Cards Won', value=str(profile.challenge_cards_won))
         em.add_field(name='Battle Deck', value=fmt)
-        # em.add_field(name='Chests', value=cycle + '\nSuper Magical: ' +
+        em.add_field(name='Chests', value=cycle + '\nSuper Magical: ' +
                      sm + '\nLegendary: ' + legend + '\nEpic: ' + epic)
 
-        em.set_thumbnail(url = profile.arena.image_url)
-        em.set_footer(text = 'Selfbot made by SharpBit | Powered by cr-api',
-                      icon_url = 'http://cr-api.com/static/img/branding/cr-api-logo.png')
+        em.set_thumbnail(url=profile.arena.image_url)
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
+                      icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
 
     @commands.command()
-    async def trophies(self, ctx, tag = None):
+    async def trophies(self, ctx, tag=None):
         '''See your current, record, and legend trophies'''
-        em=discord.Embed(title = 'Trophies')
-        em.color=await ctx.get_dominant_color(ctx.author.avatar_url)
+        em = discord.Embed(title='Trophies')
+        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
         if tag is None:
-            tag=self.tag
+            tag = self.tag
             if tag is None:
                 em.description - 'Please add `TAG` to your config.'
-                return await ctx.send(embed = em)
+                return await ctx.send(embed=em)
         try:
-            profile=await self.client.get_profile(tag)
+            profile = await self.client.get_profile(tag)
         except:
-            em.description='Either the API is down or that\'s an invalid tag.'
-            return await ctx.send(embed = em)
+            em.description = 'Either the API is down or that\'s an invalid tag.'
+            return await ctx.send(embed=em)
 
-        trophies=str(profile.current_trophies)
-        highest_trophies=str(profile.highest_trophies)
-        legend_trophies=str(profile.legend_trophies)
+        trophies = str(profile.current_trophies)
+        highest_trophies = str(profile.highest_trophies)
+        legend_trophies = str(profile.legend_trophies)
 
-        em.title=profile.name
-        em.set_author(name = 'Trophies', icon_url = profile.arena.image_url)
-        em.description='Trophies: `' + trophies + '`\nPersonal Best: `' +
+        em.title = profile.name
+        em.set_author(name='Trophies', icon_url=profile.arena.image_url)
+        em.description = 'Trophies: `' + trophies + '`\nPersonal Best: `' +
             highest_trophies + '`\nLegend Trophies: `' + legend_trophies + '`'
         em.set_thumbnail(
             url='http://vignette1.wikia.nocookie.net/clashroyale/images/7/7c/LegendTrophy.png/revision/latest?cb=20160305151655')
