c6cff81d378604dd18dbb664bc2a98e8979f7d0e
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-05 12:19:21 -0500

    remove space for aesthetics

--- cogs/clan.py
+++ cogs/clan.py
@@ -53,7 +53,7 @@ class Clan:
             rank = str(clan.rank)
 
         chest = str(clan.clan_chest.crowns) + '/' + str(clan.clan_chest.required) + \
-            f' ({(clan.clan_chest.crowns / clan.clan_chest.required) * 100:.3f} %)'
+            f' ({(clan.clan_chest.crowns / clan.clan_chest.required) * 100:.3f}%)'
         members = str(len(clan.members)) + '/50'
 
         em.title = clan.name + ' (#' + clan.tag + ')'
