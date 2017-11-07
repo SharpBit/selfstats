36e3133087992699ae222dcf19f1f267e045eb02
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-07 11:07:27 -0500

    revert clan for the time being

--- cogs/clan.py
+++ cogs/clan.py
@@ -33,6 +33,13 @@ class Clan:
 
     def __init__(self, bot):
         self.bot = bot
+        with open('data/config.json') as f:
+            config = json.load(f)
+            if 'TAG' not in config:
+                tag = None
+            else:
+                tag = config['TAG']
+        self.tag = os.environ.get('TAG') or tag
         self.cr = bot.cr
         self.conv = TagCheck()
 
@@ -60,15 +67,73 @@ class Clan:
 
     @commands.command()
     async def clan(self, ctx, clan_tag=None):
-        tag = await self.resolve_tag(ctx, clan_tag, clan=True)
+        em = discord.Embed(title='Clan Info')
+        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
+
+        if clan_tag is None:
+            tag = self.tag
+            if tag is None:
+                em.description - 'Please add `TAG` to your config.'
+                return await ctx.send(embed=em)
+            try:
+                profile = await self.client.get_profile(tag)
+            except:
+                em.description = 'Either the API is down or your tag is an invalid player tag.'
+                return await ctx.send(embed=em)
+        else:
+            clan_tag = clan_tag.strip('\#').replace('O', '0')
+            clan = await self.client.get_clan(clan_tag)
 
         try:
-            clan = await self.cr.get_clan(tag)
+            clan = await profile.get_clan()
+        except ValueError:
+            em.description = 'You are not in a clan'
+            return await ctx.send(embed=em)
         except Exception as e:
-            return await ctx.send(f'`{e}`')
+            pass
+
+        if clan.rank == 0:
+            rank = 'Unranked'
         else:
-            em = await embeds.format_clan(ctx, clan)
-            await ctx.send(embed=em)
+            rank = str(clan.rank)
+
+        chest = str(clan.clan_chest.crowns) + '/' + str(clan.clan_chest.required) + \
+            f' ({(clan.clan_chest.crowns / clan.clan_chest.required) * 100:.3f}%)'
+        members = str(len(clan.members)) + '/50'
+
+        pushers = []
+        if len(clan.members) >= 3:
+            for i in range(3):
+                pushers.append(
+                    f"**{clan.members[i].name}**\n{clan.members[i].trophies} trophies\n#{clan.members[i].tag}")
+        contributors = list(reversed(sorted(clan.members, key=lambda x: x.crowns)))
+
+        ccc = []
+        if len(clan.members) >= 3:
+            for i in range(3):
+                ccc.append(
+                    f"**{contributors[i].name}**\n{contributors[i].crowns} crowns\n#{contributors[i].tag}")
+
+        em.title = clan.name + ' (#' + clan.tag + ')'
+        em.set_author(name='Clan Info', icon_url=ctx.author.avatar_url)
+        em.description = clan.description
+
+        em.add_field(name='Score', value=str(clan.score))
+        em.add_field(name='Required Trophies', value=str(clan.required_trophies))
+        em.add_field(name='Donations', value=str(clan.donations))
+        em.add_field(name='Region', value=clan.region)
+        em.add_field(name='Global Rank', value=rank)
+        em.add_field(name='Type', value=clan.type_name)
+        em.add_field(name='Clan Chest', value=chest)
+        em.add_field(name='Members', value=members)
+        em.add_field(name='Top Players', value='\n\n'.join(pushers))
+        em.add_field(name='Top Contributors', value='\n\n'.join(ccc))
+
+        em.set_thumbnail(url=clan.badge_url)
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
+                      icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
+
+        await ctx.send(embed=em)
 
     @commands.group(invoke_without_command=True)
     async def members(self, ctx):
