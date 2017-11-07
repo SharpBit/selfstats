1003046d5a77f596240127f7c1e3040fbab4103f
SharpBit <31069084+SharpBit@users.noreply.github.com>
2017-11-07 12:08:50 -0500

    add cc cmd

--- cogs/clan.py
+++ cogs/clan.py
@@ -136,6 +136,68 @@ class Clan:
 
         await ctx.send(embed=em)
 
+    @commands.command()
+    async def clanchest(self, ctx, clan=None):
+        '''Returns clan chest info for a clan'''
+        em = discord.Embed(title='Clan Info')
+        em.color = await ctx.get_dominant_color(ctx.author.avatar_url)
+
+        if clan is None:
+            tag = self.tag
+            if tag is None:
+                em.description - 'Please add `TAG` to your config.'
+                return await ctx.send(embed=em)
+            try:
+                profile = await self.client.get_profile(tag)
+            except:
+                em.description = 'Either the API is down or your tag is an invalid player tag.'
+                return await ctx.send(embed=em)
+        else:
+            clan_tag = clan.strip('\#').replace('O', '0')
+            clan = await self.client.get_clan(clan_tag)
+
+        try:
+            clan = await profile.get_clan()
+        except ValueError:
+            em.description = 'You are not in a clan'
+            return await ctx.send(embed=em)
+        except Exception as e:
+            pass
+
+        chest = clan.clan_chest
+
+        em.title = clan.name + ' (#' + clan.tag + ')'
+        em.set_author(name='Clan Chest', icon_url=ctx.author.avatar_url)
+        em.description = clan.description
+
+        top_contributors = list(reversed(sorted(clan.members, key=lambda x: x.crowns)))
+        worst_contributors = list(sorted(clan.members, key=lambda x: x.crowns))
+        ccc = []
+        if len(clan.members) >= 3:
+            for i in range(3):
+                ccc.append(
+                    f"**{top_contributors[i].name}**\n{top_contributors[i].crowns} crowns\n#{top_contributors[i].tag}")
+
+        wc = []
+        if len(clan.members) >= 3:
+            for i in range(3):
+                wc.append(
+                    f'**{worst_contributors[i].name}**\n{worst_contributors[i].crowns} crowns\n#{worst_contributors[i].tag}')
+
+        em.add_field(name='Crowns Earned', value=f'{chest.crowns}')
+        em.add_field(name='Required Crowns', value=f'{chest.required}')
+        em.add_field(name='Percent Earned',
+                     value=f'({(clan.clan_chest.crowns / clan.clan_chest.required) * 100:.3f}%)')
+        em.add_field(name='Percent Required', value=f'{chest.percent:.3f}%')
+        em.add_field(name='Top Contributors', value='\n\n'.join(ccc))
+        em.add_field(name='Worst Contributors', value='\n\n'.join(wc))
+
+        em.set_thumbnail(clan.badge_url)
+        em.set_footer(text='Selfbot made by SharpBit | Powered by cr-api',
+                      icon_url='http://cr-api.com/static/img/branding/cr-api-logo.png')
+
+        await ctx.send(embed=em)
+
     @commands.group(invoke_without_command=True)
     async def members(self, ctx):
         '''A command group that finds the worst and best members in a clan'''
