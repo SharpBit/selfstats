6434ce9489c81958fa3c39aec4c400adea8350ee
Jason <31069084+SharpBit@users.noreply.github.com>
2017-11-14 16:33:52 -0500

    Add quote

--- cogs/utility.py
+++ cogs/utility.py
@@ -75,7 +75,7 @@ class Utility:
 
         for embed in pages:
             em = discord.Embed(title='Command Help', color=color)
-            embed += f'\n\nType {ctx.prefix}help command for more info on a command.\nYou can also type {ctx.prefix}help category for more info on a category.
+            embed += f'\n\nType {ctx.prefix}help command for more info on a command.\nYou can also type {ctx.prefix}help category for more info on a category.'
             em.description = embed
             try:
                 await ctx.send(embed=em)
